44187598880c4fb7258c133b18af9b0138ac3092
Presti <dxssucuk@hotmail.com>
2024-08-30 16:12:24 +0200

    Bug fix.

---
+++ src/main/java/de/presti/heartmybeatonline/controller/ErrorController.java
@@ -0,0 +1,31 @@
+package de.presti.heartmybeatonline.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    /**
+     * Error Attributes in the Application
+     */
+    private final ErrorAttributes errorAttributes;
+
+    /**
+     * Controller for the Error Controller
+     * @param errorAttributes Attributes that give more Info about the Error.
+     */
+    public ErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    /**
+     * Request mapper for errors.
+     * @return {@link String} for Thyme to the HTML Page.
+     */
+    @GetMapping(value = "/error")
+    public String error(WebRequest webRequest) {
+        return "error";
+    }
+}
