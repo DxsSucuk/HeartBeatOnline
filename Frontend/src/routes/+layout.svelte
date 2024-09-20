595a342c18efaee34f757b7ede70b0a398f5448f
Presti <dxssucuk@hotmail.com>
2024-09-20 17:11:33 +0200

    Update +layout.svelte

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -28,6 +28,15 @@
 
 	initializeStores();
 </script>
+<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
+<script>
+  kofiWidgetOverlay.draw('presti', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#794bc4',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
+</script>
 
 <Toast />
 <Modal />
